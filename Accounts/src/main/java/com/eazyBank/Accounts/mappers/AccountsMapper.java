3580b20d1ce267d621723462ae0500e99a16ffcf
Arjunagi A Rehman <abdul123arj@gmail.com>
2023-12-20 01:28:13 +0530

    added docker

--- Accounts/src/main/java/com/eazyBank/Accounts/mappers/AccountsMapper.java
+++ Accounts/src/main/java/com/eazyBank/Accounts/mappers/AccountsMapper.java
@@ -2,8 +2,9 @@ package com.eazyBank.Accounts.mappers;
 
 import com.eazyBank.Accounts.Dtos.AccountsDto;
 import com.eazyBank.Accounts.models.Accounts;
+import org.springframework.beans.factory.BeanFactory;
 
-public class AccountsMapper {
+public class AccountsMapper   {
     public static AccountsDto mapToAccountsDto( Accounts accounts,AccountsDto accountsDto){
         accountsDto.setAccount_number(accounts.getAccount_number());
         accountsDto.setAccountType(accounts.getAccountType());
